8a2b11e64335b0cec015fc6222cd2bf984527859
Ali Malik <helloimali@users.noreply.github.com>
2025-03-26 21:36:25 +0000

    Counter

--- 01_03b.py
+++ 01_03b.py
@@ -1,8 +1,12 @@
+from collections import Counter
+
 def main():
     #Add code here
     inventory = Counter(STA001=10, SAL002=20, ENT004=13)
     #sell 5 starters, 3 salads, and 3 entrees
+    inventory.subtract(STA001=5,SAL002=3,ENT004=3)
     #make 9 more starters and 1 more entree
+    inventory.update(STA001=9,SAL002=0,ENT004=1)
     print(inventory)
 
 if __name__ == "__main__":
