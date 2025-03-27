ae7b533b611863a81992872b333f5b978413e567
Ali Malik <helloimali@users.noreply.github.com>
2025-03-27 07:34:30 +0000

    csv

--- 03_01b.py
+++ 03_01b.py
@@ -1,9 +1,27 @@
+from collections import namedtuple
+
+def can_we_take_the_order(driver, value):
+    # if driver.index(2) > value:
+    #     return True
+    # return False
+    return driver.car_capacity >= value
 
 def main():
     #add code here
-    #create a driver with a name, car type, and car capacity
-    #an example you can use to practice: "Iris", "Toyota Prius", 7
-    #check if they can take a certain order, given their car's capacity.
+    # #create a driver with a name, car type, and car capacity
+    # #an example you can use to practice: "Iris", "Toyota Prius", 7
+    # driver = namedtuple("Iris", "Toyota Prius", 7)
+    
+    # #check if they can take a certain order, given their car's capacity.
+    # print(can_we_take_the_order(driver, 3))
+    # print(can_we_take_the_order(driver, 13))
+
+    Driver = namedtuple("driverrrrrr", ["name", "car_type", "car_capacity"])
+    iris = Driver("Iris", "Prius", 7)
+    mat = Driver("Mat", "SUV", 27)
+    sam = Driver("Sam", "bike", 1)
+    print(can_we_take_the_order(iris, 2))
+    print(can_we_take_the_order(iris, 20))
     return
 
 if __name__ == "__main__":
--- 03_02b.py
+++ 03_02b.py
@@ -1,8 +1,17 @@
+from collections import namedtuple
+import csv
 
 def main():
     #add code here
     #read data/Customer.csv
-    #Create workable objects with each line
+    with open("data/Customer.csv", "r") as f:
+        reader = csv.reader(f)
+        # Picks up the header and creates Person() tuple
+        Person = namedtuple("Person", next(reader))
+        for read in reader:
+                person = Person(*read)
+                print(person)
+
     return
 
 if __name__ == "__main__":
